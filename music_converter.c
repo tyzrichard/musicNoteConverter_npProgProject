5690be140ea1a21a90ff42f6e635e69db922f337
tyzrichard <tyzrichard28@gmail.com>
2024-05-15 16:59:22 +0800

    Bug Fixes

--- music_converter.c
+++ music_converter.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-main () {
+void main (void) {
     // Array for Music Notes. Its the only bit not covered in syllabus, but I really couldn't think of any other way to do it.
     // I could have used if-else statements, but ehhhhhh
-    char notes[] = {'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C'}
+    //char notes[] = {'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C'};
 
-    char initialNote, convertedNote;
+    char initialNote;
     printf("Welcome! Enter a note to be converted (No flats, only whitekeys and sharps): ");
-    scanf(' %c', &initialNote);
+    scanf(" %c", &initialNote);
 
-    printf("%c", initialNote)
+    printf("%c", initialNote);
 
 
 }
