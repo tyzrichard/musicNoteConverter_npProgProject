565b06107e7bdc4b71127d10e5fa43fc0e60ebbe
tyzrichard <tyzrichard28@gmail.com>
2024-05-27 13:09:44 +0800

    Allow notes to be converted from C Major to E Major with respective output

--- music_converter.c
+++ music_converter.c
@@ -1,18 +1,66 @@
 #include <stdio.h>
 
 void main () {
-    char inputNote, sharpFlat, key;
+    char inputNote, sharpFlat, key, finalNote, finalSharpFlat;
 
     printf("Welcome! Enter a note (C, D, E, F, G, A, B): ");
     scanf(" %c", &inputNote);
-    printf("Does your note have a sharp/flat? Enter # for sharp, b for flat, and press enter if it has none of the mentioned: ");
+    printf("\nDoes your note have a sharp/flat? Enter # for sharp, b for flat, and any other key if it has none of the mentioned: ");
     scanf(" %c", &sharpFlat);
 
-    printf("From C Major, which key would you like to convert this note to?");
-    printf("Enter E for E Major, C for C# Minor, A for Ab Major, and F for F Minor");
-    scanf(" %c", &key)
+    printf("\nFrom C Major, which key would you like to convert this note to?");
+    printf("\nEnter E for E Major, C for C# Minor, A for Ab Major, and F for F Minor");
+    scanf(" %c", &key);
 
-    if (key == G) {
-        if (inputNote == "C" ||)
+    if (key == 'E') { //Everything is shifted up 4 semitones
+        if ((inputNote == 'C' && (sharpFlat != '#' || sharpFlat != 'b')) || (inputNote == 'B' && sharpFlat == '#')) { //C, aka B#
+            finalNote = 'E';
+        } else if ((inputNote == 'C' && sharpFlat == '#') || (inputNote == 'D' && sharpFlat == 'b')) { //C#, aka Db
+            finalNote = 'F';
+        } else if (inputNote == 'D' && (sharpFlat != '#' || sharpFlat != 'b')) { //D
+            finalNote = 'F';
+            finalSharpFlat = '#';
+        } else if ((inputNote == 'D' && sharpFlat == '#') || (inputNote == 'E' && sharpFlat == 'b')) { //D#, aka Eb
+            finalNote = 'G';
+        } else if ((inputNote == 'E' && (sharpFlat != '#' || sharpFlat != 'b')) || (inputNote == 'F' && sharpFlat == 'b')) { //E, aka Fb
+            finalNote = 'G';
+            finalSharpFlat = '#';
+        } else if ((inputNote == 'F' && (sharpFlat != '#' || sharpFlat != 'b')) || (inputNote == 'E' && sharpFlat == '#')) { //F, aka E#
+            finalNote = 'A';
+        } else if ((inputNote == 'F' && sharpFlat == '#') || (inputNote == 'G' && sharpFlat == 'b')) { //F#, aka Gb
+            finalNote = 'A';
+            finalSharpFlat = '#';
+        } else if (inputNote == 'G' && (sharpFlat != '#' || sharpFlat != 'b')) { //G
+            finalNote = 'B';
+        } else if ((inputNote == 'G' && sharpFlat == '#') || (inputNote == 'A' && sharpFlat == 'b')) { //G#, aka Ab
+            finalNote = 'C';
+        } else if (inputNote == 'A' && (sharpFlat != '#' || sharpFlat != 'b')) { //A
+            finalNote = 'C';
+            finalSharpFlat = '#';
+        } else if ((inputNote == 'A' && sharpFlat == '#') || (inputNote == 'B' && sharpFlat == 'b')) { //A#, aka Bb
+            finalNote = 'D';
+        } else if ((inputNote == 'B' && (sharpFlat != '#' || sharpFlat != 'b')) || (inputNote == 'C' && sharpFlat == 'b')) { //B, aka Cb
+            finalNote = 'D';
+            finalSharpFlat = '#';
+        } else {
+            printf("\nThe note %c%c either doesn't exist, or isn't within the keys that this program can convert currently.\n", inputNote, sharpFlat);
+            printf("Perhaps you might want to run this program again?\n");
+        }
+    } else if (key == 'C') {
+        printf("NOT YET");
+    } else if (key == 'A') {
+        printf("NOT YET");
+    } else if (key == 'F') {
+        printf("NOT YET");
+    } else {
+        printf("\nThe key %c either doesn't exist, or isn't within the keys that this program can convert currently.\n", key);
+        printf("Perhaps you might want to run this program again?\n");
     }
+
+    if (sharpFlat == '#' || sharpFlat == 'b') {
+        printf("The key %c%c when converted from C Major to E Major is %c%c.", inputNote, sharpFlat, finalNote, finalSharpFlat);
+    } else {
+        printf("The key %c when converted from C Major to E Major is %c%c.", inputNote, finalNote, finalSharpFlat);
+    }
+    
 }
